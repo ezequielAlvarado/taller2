bb30bd53e53d5ce02fc50542ab0eb5d7f6e8778a
PatricioS <patriciosalvador@live.com.ar>
2019-09-19 20:41:38 -0300

    mejorando-pagina

--- src/app.py
+++ src/app.py
@@ -3,9 +3,13 @@ from flask import render_template
 
 app = Flask(__name__)       
 
-@app.route("/")
+@app.route("/home")
 def main():
     return render_template('home.html')
+
+@app.route('/history')
+def hist():
+    return render_template('history.html')
     
 if __name__ == "__main__":
     app.run("0.0.0.0", port=4000, debug=True)
