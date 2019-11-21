64fce834a5374eaca6e6ec5628665c57c0833bf0
PatricioS <patriciosalvador@live.com.ar>
2019-11-20 21:39:34 -0300

    ajax por socketio en history

--- src/apphist.py
+++ src/apphist.py
@@ -1,16 +1,13 @@
 # coding=utf-8
 from flask import Flask, redirect, url_for
-from flask import render_template  
-from flask_socketio import SocketIO
+from flask import render_template, request, jsonify
 import schemadb
 import time
 import json
 
 tiempo = 3600 # Una hora atras
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
-socketio = SocketIO(app)
 
 @app.route('/history')
 def hist():
@@ -38,8 +35,35 @@ def hist():
         return render_template('history.html', date=date, temp=temp, hum=hum, cant=N)
 
 
-@socketio.on('intervalo')
-def handle_intervalo(inter):
+@app.route('/history_dat', methods= ['POST'])
+def get_d():
+    global tiempo
+    ts = time.time()
+    ts = int(ts)
+    ts2 = ts - tiempo
+    #leo el historial de la bd 
+    x = schemadb.busc_tiempo(ts2)
+    N = x.count()
+    print(x)
+    date = []
+    temp = []
+    hum = []
+    print(N)
+    print("llega")
+    if(N == 0):
+        print("No hay resultados.")
+        return jsonify({'date' : 0, 'hum': 0, 'temp' : 0, 'cant': N})
+    else:
+        for item in x:
+            date.append(item["Date"]) #agrego valores ya que date[0] no existe al crearlo vacio
+            temp.append(item["Temp"])
+            hum.append(item["Hum"]) 
+        return jsonify({'date' : date, 'hum': hum, 'temp' : temp, 'cant': N})
+
+@app.route('/history_sel', methods=['POST'])
+def handle_intervalo():
+    inter = request.values.get('selected')
+    print(inter)
     print ("Intervalo: " + inter)
     global tiempo
     if(inter == "H"):
@@ -50,8 +74,9 @@ def handle_intervalo(inter):
         tiempo = 604800 #Una semana atras.
     if(inter == "Y"):
         tiempo = (86400*365) #Un año atras.
-    socketio.emit('reload')
+    print(tiempo)
+    get_d()
 
 if __name__ == "__main__":
     print(schemadb.connectbd())
-    socketio.run(app,debug=True)
+    app.run(host='localhost', port=5000, debug=True)
--- src/static/scripts.js
+++ src/static/scripts.js
@@ -1,83 +1,106 @@
+var int;
+
+function x(){
+    var c, date, h, t;
+    $.ajax({
+        url: '/history_dat',
+        type : 'POST',
+        success: function( data ) {
+            c = data['cant'];
+            date = data['date'];
+            if(c > 0){
+                for(var i = 0; i<c; i++){
+                    var dat = new Date(date[i]*1000);
+                    date[i]=dat.toLocaleString("es-AR");
+                }
+            }
+            console.log(date);
+            h = data['hum'];
+            t = data['temp'];
+            tipo = "Temperature";
+            tipo2 = "Humidity";
+            grafico(date,t,c,tipo);
+            grafico(date,h,c,tipo2);
+        }
+    });
+    clearInterval(int);
+}
+
+
 
 function tiempo() {
     var selectBox = document.getElementById("seleccion");
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
     console.log(selectedValue);
-    const socket = io();
-    socket.emit('intervalo', inter = selectedValue);
+    $.post( "/history_sel", { selected: selectedValue});
+    startinterval();
 }
 
-
+function startinterval(){
+    int = setInterval(x,1000);
+}
 
 function grafico(date,data,cant,tipo){
-    console.log(cant);
-    if(cant != 0){
-        if(tipo == "Temperature"){
-            Max = 60;
-            Min = 0;
-            Step = 5;
-            var ctxL = document.getElementById("myChart1").getContext('2d');
-            var datos = {
-                labels: date,
-                datasets: [{
-                    label: "Temperature",
-                    data: data,
-                    backgroundColor: [
-                        'rgba(228, 10, 14, .2)',
-                    ],
-                    borderColor: [
-                        'rgba(228, 10, 14, .7)',
-                    ],
-                    borderWidth: 3
-                    }
-                ]
-            };
-        }
-        if(tipo == "Humidity"){
-            Max = 100;
-            Min = 0;
-            Step = 10;
-            var ctxL = document.getElementById("myChart2").getContext('2d');
-            var datos = {
-                labels: date,
-                datasets: [
-                    {
-                    label: "Humidity",
-                    data: data,
-                    backgroundColor: [
-                        'rgba(10, 119, 228, .1)',
-                    ],
-                    borderColor: [
-                        'rgba(10, 119, 228, .7)',
-                    ],
-                    borderWidth: 3
-                    }
-                ]
-            }
-        }
-        for(var i = 0; i<cant; i++){
-            var dat = new Date(date[i]*1000);
-            date[i]=dat.toLocaleString("es-AR");
-        }
-        var chartopt = {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        max: Max,
-                        min: Min,
-                        stepSize: Step
-                    }
-                }]
-            }
+    console.log("pollopa" + cant);
+    if(tipo == "Temperature"){
+        Max = 60;
+        Min = 0;
+        Step = 5;
+        var ctxL = document.getElementById("myChart1").getContext('2d');
+        var datos = {
+            labels: date,
+            datasets: [{
+                label: "Temperature",
+                data: data,
+                backgroundColor: [
+                    'rgba(228, 10, 14, .2)',
+                ],
+                borderColor: [
+                    'rgba(228, 10, 14, .7)',
+                ],
+                borderWidth: 3
+                }
+            ]
         };
-        var myLineChart = new Chart(ctxL, {
-            type: 'line',
-            data: datos,
-            options: chartopt            
-        });
-    }    
-    else{
-        document.getElementById("graf1").innerHTML= '<h1> No existen mediciones recientes de temperatura </h1>';
-        document.getElementById("graf2").innerHTML= '<h1> No existen mediciones recientes de humedad </h1>';
-    }        
-}
+    }
+    if(tipo == "Humidity"){
+        Max = 100;
+        Min = 0;
+        Step = 10;
+        var ctxL = document.getElementById("myChart2").getContext('2d');
+        var datos = {
+            labels: date,
+            datasets: [
+                {
+                label: "Humidity",
+                data: data,
+                backgroundColor: [
+                    'rgba(10, 119, 228, .1)',
+                ],
+                borderColor: [
+                    'rgba(10, 119, 228, .7)',
+                ],
+                borderWidth: 3
+                }
+            ]
+        }
+    }
+    var chartopt = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: Max,
+                    min: Min,
+                    stepSize: Step
+                }
+            }]
+        }
+    };
+    var myLineChart = new Chart(ctxL, {
+        type: 'line',
+        data: datos,
+        options: chartopt            
+    });
+}   
+
+
