6d588822aecb4c5a0969748c174b427cc4d2ee06
PatricioS <patriciosalvador@live.com.ar>
2019-11-21 17:50:43 -0300

    Para probar todo

--- src/subscriber.py
+++ src/subscriber.py
@@ -353,7 +353,7 @@ def handle_intervalo():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8888, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 while True:
 	time.sleep(1)
